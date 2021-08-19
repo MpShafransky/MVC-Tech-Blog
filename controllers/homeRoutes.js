210c3bd3f9ce3482b974ab51c6c1ad7fd8ef4dfb
Matthew Shafransky <matt@Matthews-MacBook-Pro.local>
2021-08-19 10:51:27 -0400

    code added and bugs fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -58,6 +58,10 @@ router.get('/addComment/:id', async (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
+    if (!req.session.user_id
+    ){
+        return res.status(403).send("Access Denied")
+    }
     try {
         const blogData = await Blog.findAll({
             where: {
